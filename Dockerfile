1db80fa25779dcb391eae0b6c1a413d8e5629383
jeffreysapia <jeffsapia1988@gmail.com>
2026-04-27 09:23:51 -0400

    Adding environments

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM nginx:alpine
+COPY /dist/moup-ui/browser /usr/share/nginx/html
+COPY /nginx.conf /etc/nginx/nginx.conf
+# Expose port 80 and start Nginx
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
+
+#ng build --configuration=production
+#docker build -t moup-ui .
+#docker kill moup-ui
+#docker rm moup-ui
+#docker run -d --name moup-ui -p 80:80 --restart always moup-ui
--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,60 +1,23 @@
 import {Component, OnInit} from '@angular/core';
-import {MediaPlayer} from './media-player/media-player';
 import {ContentCard} from './content-card/content-card';
 import {HttpClient} from '@angular/common/http';
-import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-main',
   imports: [
-    ContentCard,
-    AsyncPipe
+    ContentCard
   ],
   templateUrl: './main.html',
   styleUrl: './main.sass',
 })
 export class Main implements OnInit {
 
-  public content$: any;
   public contentList: any = [];
-  public contentList1: any = [
-    {
-      "uuid": "12fbfed7-6c20-4538-bbca-eecaf5fea92e",
-      "title": "How To Get To Heaven - Episode #1",
-      "description": "This is a test of the How To Get To Heaven Show in which we do just that, guide souls to Heaven.",
-      "uploadedOn": "2026-04-23T21:53:14.307322Z",
-      "duration": 44.544,
-      "filename": "2026-04-23-how-to-get-to-heaven---episode-#1.m4a"
-    },
-    {
-      "uuid": "99a18dcb-3a72-4a46-bbe6-14461bf704b2",
-      "title": "How To Get To Heaven - Episode #1",
-      "description": "This is a test of the How To Get To Heaven Show in which we do just that, guide souls to Heaven.",
-      "uploadedOn": "2026-04-23T20:21:39.330070Z",
-      "duration": 44.544,
-      "filename": "2026-04-23-how-to-get-to-heaven---episode-#1.m4a"
-    },
-    {
-      "uuid": "12d85829-fa38-4bee-9f62-243c5d175b38",
-      "title": "How To Get To Heaven - Episode #1",
-      "description": "This is a test of the How To Get To Heaven Show in which we do just that, guide souls to Heaven.",
-      "uploadedOn": "2026-04-23T19:46:05.695639Z",
-      "duration": 44.544,
-      "filename": "2026-04-23-how-to-get-to-heaven---episode-#1.m4a"
-    },
-    {
-      "uuid": "4764a30c-57c7-4076-9a3b-5e6ab20ad49c",
-      "title": "How To Get To Heaven - Episode #1",
-      "description": "This is a test of the How To Get To Heaven Show in which we do just that, guide souls to Heaven.",
-      "uploadedOn": "2026-04-23T18:35:38.124795Z",
-      "duration": 44.544,
-      "filename": "2026-04-23-how-to-get-to-heaven---episode-#1.m4a"
-    }
-  ]
-  ;
 
   constructor(private http: HttpClient) {
-    this.http.get('http://localhost:8080/content')
+    const apiUrl = environment.apiUrl;
+    this.http.get(apiUrl + '/content')
       .subscribe((contentList : any) => {
         this.contentList = contentList;
         console.log(JSON.stringify(this.contentList));
