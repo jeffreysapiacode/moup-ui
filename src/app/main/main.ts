f8e081e97c530f745f20aee5ee95775af5dc8985
jeffreysapia <jeffsapia1988@gmail.com>
2026-05-11 16:35:16 -0400

    Adding timeout for content api fetch request

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -7,6 +7,7 @@ import {NgClass} from '@angular/common';
 import {AudioGlobal} from '../service/audio-global';
 import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 import {EventBus} from '../service/event-bus';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -24,6 +25,9 @@ export class Main implements OnInit {
   loading: boolean = false;
   apiUrl = environment.apiUrl;
   error: boolean = false;
+  statusCode: any;
+  waitForResponse: boolean = false;
+  contentSubscription: Subscription | undefined;
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
@@ -32,6 +36,7 @@ export class Main implements OnInit {
 
   constructor(protected http: HttpClient,
               protected audioGlobal: AudioGlobal,
+              protected eventBus: EventBus,
               protected cdr: ChangeDetectorRef) {
   }
 
@@ -39,17 +44,51 @@ export class Main implements OnInit {
     this.innerWidth = window.innerWidth;
     this.loading = true;
     this.error = false;
-    this.http.get(this.apiUrl + '/content')
-      .subscribe((contentList: any) => {
-        this.audioGlobal.contentList = contentList;
-        setTimeout(() => {
-          this.loading = false;
-          this.cdr.detectChanges();
-        }, 2000);
-      }, (error) => {
+    this.statusCode = null;
+    this.waitForResponse = false;
+    // Initiate timeout for minimum response time
+    setTimeout(() => {
+      if (this.statusCode === 200) {
+        this.loading = false;
+        this.cdr.detectChanges();
+      } else {
+        this.waitForResponse = true;
+        this.waitForResponseLoop();
+      }
+    }, 2000);
+    // Cancel subscription after timeout
+    const timeoutId = setTimeout(()=>{
+      if (this.waitForResponse) {
+        this.contentSubscription?.unsubscribe();
+        this.waitForResponse = false;
+        this.loading = false;
+      }
+    }, 10000);
+    this.contentSubscription = this.http.get(this.apiUrl + '/content', { observe: 'response' })
+      .subscribe((response: any) => {
+        this.statusCode = response.status;
+        if (this.statusCode === 200) {
+          clearTimeout(timeoutId);
+          console.log('StatusCode: ', this.statusCode);
+          this.audioGlobal.contentList = response.body;
+        }
+      }, (error: any) => {
         this.error = true;
       }, () => {
         this.loading = false;
       });
   }
+
+  waitForResponseLoop() {
+    if (this.waitForResponse) {
+      if (this.statusCode === 200) {
+        this.waitForResponse = false;
+        this.loading = false;
+        this.cdr.detectChanges();
+      }
+      requestAnimationFrame(this.waitForResponseLoop.bind(this));
+    }
+  }
+
 }
+
